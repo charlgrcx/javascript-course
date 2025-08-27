73b8e6c752ef1c3c8cbdb5c57afe188e13d002e9
charlgrcx <navidadcharlyngrace@gmail.com>
2025-08-27 17:17:55 +0800

    feat: add functions with parameters and return values
    - Implement function declarations and expressions
    - Add parameters and arguments for flexible functions
    - Create return values for reusable calculations
    - Establish function scope and variable accessibility
    - Complete gymnastics team calculator challenge with professional function organization

---
+++ 02-fundamentals-part-2/starter/script.js
@@ -0,0 +1,100 @@
+console.log("Part 2 function is ready.");
+
+// Functions - Declarations and Expressions
+function logger() {
+    console.log(`My name is Charlyn!`);
+}
+
+logger ();
+logger ();
+logger ();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`
+    return juice;
+}
+
+console.log(fruitProcessor(11, 3));
+
+// const juice1 = `Juice with 5 apples and 0 oranges.`;
+// const juice2 = `Juice with 2 apples and 4 oranges.`;
+// const juice3 = `Juice with 3 apples and 2 oranges.`;
+
+const juice1 = fruitProcessor(5, 0);
+const juice2 = fruitProcessor(2, 4);
+const juice3 = fruitProcessor(3,2);
+
+// Function expression
+const calcAge = function (birthYear) {
+    return 2025 - birthYear;
+}
+
+const age1 = calcAge(2004);
+console.log(age1);
+
+console.log(calcAge(1991));
+
+function introduce(firstName, lastName, age) {
+    const introduction = `Hi, I'm ${firstName} ${lastName} and I'm ${age}`;
+    return introduction;
+}
+
+console.log(introduce(`Charlyn`, `Navidad`, 20));
+console.log(introduce(`John`));
+
+function yearsUntilRetirement(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        return `${firstName} retires in ${retirement} years`;
+    } else {
+        return `${firstName} has already retired`;
+    }
+}
+console.log(yearsUntilRetirement(2004, `Charl`));
+
+
+
+/// function scope
+const globalVar = "I am global";
+
+function testScope() {
+    const localVar = "I am local";
+    console.log(globalVar);
+    console.log(localVar);
+}
+
+testScope();
+console.log(globalVar);
+//console.log(localVar);
+
+////////////////////////////////////
+// Coding Challenge #1
+
+// Function to calculate average of 3 scores
+function calcAverage(score1, score2, score3) {
+  return (score1 + score2 + score3) / 3;
+}
+
+// Function to check winner
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+  } else {
+    return `No team wins (Dolphins: ${avgDolphins}, Koalas: ${avgKoalas})`;
+  }
+}
+
+// Test Data 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// Test Data 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(checkWinner(scoreDolphins, scoreKoalas));
