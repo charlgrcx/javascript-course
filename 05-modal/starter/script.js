957c8e26e2106b7c14bf311d04dfc4d05fae46b5
charlgrcx <navidadcharlyngrace@gmail.com>
2025-09-20 16:04:41 +0800

    feat(modal): modal open/close via class toggling and DRY helpers

--- 05-modal/starter/script.js
+++ 05-modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// modal elements
+const modalEl = document.querySelector('.modal');
+
+// overlay modal
+const overlayEl = document.querySelector('.overlay');
+
+// close modal button
+const btnCloseModal = document.querySelector('.close-modal');
+
+
+console.log('Open buttons: ', btnsOpenModalEl.length);
+
+const openModal = function () {
+    // remove hidden classname in modal
+    modalEl.classList.remove('hidden');
+
+    // remove hidden classname in overlay
+    overlayEl.classList.remove('hidden');
+};
+
+
+const closeModal = function () {
+    modalEl.classList.add('hidden');
+
+    overlayEl.classList.add('hidden');
+};
+
+
+btnsOpenModalEl.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
